eaff9f5a4e86c3d661a54205170a35d566adf520
Md Danish Afsar <danishsd2021@gmail.com>
2026-08-24 01:13:28 +0530

    Add new changes

--- Module/Azure_Storage_Account/variable.tf
+++ Module/Azure_Storage_Account/variable.tf
@@ -1,1 +1,9 @@
-variable "storage"{}
+variable "storage"{
+    type = object({
+        name                = string
+        resource_group_name = string
+        location            = string
+        account_tier        = string
+        account_replication_type = string
+    })
+}
